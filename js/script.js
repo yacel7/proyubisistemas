ffee0dc96f40df5023158d91e73bf40f2ee473c2
yacel7 <yaderforgiven@gmail.com>
2021-10-15 23:57:25 -0400

    ubi

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// init Isotope
+var $grid = $('.marcas-list').isotope({
+
+});
+// filtrar elementos al hacer clic en el botón
+$('.filter-button-group').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  resetFilterBtns();
+  $(this).addClass('active-filter-btn');
+  $grid.isotope({ filter: filterValue });
+});
+
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns(){
+  filterBtns.each(function(){
+    $(this).removeClass('active-filter-btn');
+  });
+}
